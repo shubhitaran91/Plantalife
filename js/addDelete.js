0fdf7c60b7fbd3e2ec8010c21ff5563d04be6f7a
Kunal Arora <kunal.arora@rapidqube.com>
2019-12-21 10:58:50 +0530

    updated

--- js/addDelete.js
+++ js/addDelete.js
@@ -1,59 +1,45 @@
-$("#btnSubmit").click(function(event) {
-  event.preventDefault();
 
-  var form = $("#fileUploadForm")[0];
 
-  var data = new FormData(form);
+$(document).ready(function () {
 
-  $('#fileUploadForm')[0].reset();
+  $('#loading').hide();
+  $('#loading').css({ 'position': '' })
 
-  $('#loading').css({'position': 'fixed'})
+  $("#btnSubmit").click(function (event) {
+    event.preventDefault();
 
-  
+    var form = $("#fileUploadForm")[0];
 
-  $.ajax({
-    type: "POST",
-    enctype: "multipart/form-data",
-    url: "https://plantalife-backend.herokuapp.com/uploadPlantData",
+    var data = new FormData(form);
 
-    data: data,
-    processData: false,
-    contentType: false,
-    cache: false,
-    success: function(data) {
-      alert(data.message);
+    $('#fileUploadForm')[0].reset();
 
-      //location.reload();
-      // window.location.href = "/listofplants.html";
-    },
-    error: function(e) {
-      console.log("ERROR : ", e);
-      $('#loading').css({'position': ''})
-    }
+    $('#loading').show();
+    $('#loading').css({ 'position': 'fixed' });
+
+    $.ajax({
+      type: "POST",
+      enctype: "multipart/form-data",
+      url: "https://plantalife-backend.herokuapp.com/uploadPlantData",
+
+      data: data,
+      processData: false,
+      contentType: false,
+      cache: false,
+      success: function (data) {
+        $('#loading').hide();
+        $('#loading').css({ 'position': '' });
+        alert(data.message);
+
+        //location.reload();
+        // window.location.href = "/listofplants.html";
+      },
+      error: function (e) {
+        $('#loading').hide();
+        $('#loading').css({ 'position': '' });
+        console.log("ERROR : ", e);
+      }
+    });
   });
-});
 
-$(document).ready(function() {
-  // var row = "";
-  // $('#dataTable').DataTable({
-  //     "ajax":{
-  //         "url": "https://plantalife-backend.herokuapp.com/getPlantData",
-  //         "type": "GET",
-  //         "datatype": "json",
-  //         "data": {},
-  //         success: function (data) {
-  //             console.log(data.message);
-  //             var len = data.message.length;
-  //             console.log(len)
-  //             for (var i = 0; i < len; i++) {
-  //                 row += "<tr><td>" + data.message[i].plant_type + "</td>" + "<td>" + data.message[i].plant_name + "</td>" + "<td>" + data.message[i].plant_price + "</td> " + "<td>" + data.message[i].plant_status + "</td> " + "<td> Edit/Delete  </td></tr> ";
-  //             };
-  //             $("#tbDetails").append(row);
-  //         }
-  //     }
-  // });
 });
-
-// document.getElementById("myButton").onclick = function () {
-//     location.href = "listofplants.html";
-// };
