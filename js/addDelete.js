4b003a605ce1bdd4e73a88106bcaeb094f5c08ef
Kunal Arora <kunal.arora@rapidqube.com>
2019-12-23 18:25:44 +0530

    updated

--- js/addDelete.js
+++ js/addDelete.js
@@ -41,7 +41,7 @@ $(document).ready(function () {
         $('#loading').hide();
         $('#loading').css({ 'position': '' });
         console.log("ERROR : ", e);
-        Notiflix.Notify.Failure("ERROR : " + e);
+        Notiflix.Notify.Failure("Some Error Occured");
       }
     });
   });
