a3944a77bbb3128cd99ed0f745239511b7d713c7
Kunal Arora <kunal.arora@rapidqube.com>
2019-12-31 17:28:29 +0530

    list

--- js/listofplants.js
+++ js/listofplants.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
                 var len = data.message.length;
                 console.log(len)
                 for (var i = 0; i < len; i++) {
-                    row += "<tr><td>" + data.message[i].plant_type + "</td>" + "<td>" + data.message[i].plant_name + "</td>" + "<td>" + data.message[i].plant_price + "</td> " + "<td>" + data.message[i].plant_status + "</td> " + '<td><button>Edit</button> / <button onclick="'+productDelete()+'">Delete</button></td></tr>'
+                    row += "<tr><td>" + data.message[i].plant_type + "</td>" + "<td>" + data.message[i].plant_name + "</td>" + "<td>" + data.message[i].plant_price + "</td> " + "<td>" + data.message[i].plant_status + "</td> " + '<td><i class="fa fa-edit"></i> / <button class="wow" onclick="'+productDelete()+'"><i class="fa fa-trash"></i></button></td></tr>'
     
                 };
                
