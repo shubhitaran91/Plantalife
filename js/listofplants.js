9d3e7aa72d259ba8b7454c09be5f53a82d4c1503
shubhitaran91 <shubhitaran@gmail.com>
2020-01-02 15:32:18 +0530

    index

--- js/listofplants.js
+++ js/listofplants.js
@@ -1,35 +1,289 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
     
     
-    var row = "";
-    $('#dataTable').DataTable({
-        "ajax":{
-            "url": "https://plantalife-backend.herokuapp.com/getPlantData",
-            "type": "POST",
-            "datatype": "json",
-            "data": {},
-            success: function (data) {
-                console.log(data.message);
-                var len = data.message.length;
-                console.log(len)
-                for (var i = 0; i < len; i++) {
-                    row += "<tr><td>" + data.message[i].plant_type + "</td>" + "<td>" + data.message[i].plant_name + "</td>" + "<td>" + data.message[i].plant_price + "</td> " + "<td>" + data.message[i].plant_status + "</td> " + '<td><button>Edit</button> / <button onclick="'+productDelete()+'">Delete</button></td></tr>'
+//     var row = "";
+//     $('#dataTable').DataTable({
+//         "ajax":{
+//             "url": "https://plantalife-backend.herokuapp.com/getPlantData",
+//             "type": "POST",
+//             "datatype": "json",
+//             "data": {},
+//             success: function (data) {
+//                 console.log(data.message);
+//                 var len = data.message.length;
+//                 console.log(len)
+//                 for (var i = 0; i < len; i++) {
+//                     row += "<tr><td>" + data.message[i].plant_type + "</td>" + "<td>" + data.message[i].plant_name + "</td>" + "<td>" + data.message[i].plant_price + "</td> " + "<td>" + data.message[i].plant_status + "</td> " + '<td><button>Edit</button> / <button onclick="'+productDelete(data.message[i].plant_no)+'">Delete</button></td></tr>'
     
-                };
+//                 };
                
-                $("#tbDetails").append(row);
-            }
+//                 $("#tbDetails").append(row);
+//             }
     
     
     
-        }
-    });
+//         }
+//     });
     
-    function productDelete() {
-        console.log("Do you really want to delete")
-        // $(ctl).parents("tr").remove();
-      }
+//     function productDelete(plant) {
+//         console.log("Do you really want to delete")
+//         // $(ctl).parents("tr").remove();
+//       }
    
     
-})
+// })
+
+$(document).ready(function()
+{
+    var data;
+    $.ajax({
+        type: "POST",
+        datatype: "json",
+        async: false,
+        url: "http://localhost:5000/getPlantData",
+  
+        data: data,
+        
+        success: myCallback,
+        error: function (e) {
+          
+          console.log("ERROR : ", e);
+          Notiflix.Notify.Failure("Some Error Occured");
+        }
+        
+      });
+      function myCallback(response) {
+        data = response;
+        console.log("Inside ajax: "+data);                
+        if(data == 'No Data Found'){
+            Notiflix.Notify.Warning("No Data Found","Please Try Again","OK");
+          }
+      }
+    
+
+
+	//--->create data table > start
+	var tbl = '';
+	tbl +='<table class="table table-hover">'
+
+		//--->create table header > start
+		tbl +='<thead>';
+			tbl +='<tr>';
+			tbl +='<th>Plant Type</th>';
+			tbl +='<th>Plant Name</th>';
+			tbl +='<th>Price</th>';
+            tbl +='<th>Status</th>';
+            tbl +='<th>Action</th>';
+			tbl +='</tr>';
+		tbl +='</thead>';
+		//--->create table header > end
+
+		
+		//--->create table body > start
+		tbl +='<tbody>';
+
+			//--->create table body rows > start
+			$.each(data, function(index, val) 
+			{
+                console.log("table data",data)
+				//you can replace with your database row id
+				var row_id = data.plant_no
+console.log("plant_no",row_id)
+				//loop through ajax row data
+				tbl +='<tr row_id="'+row_id+'">';
+					tbl +='<td ><div class="row_data" edit_type="click" col_name="plant_type">'+val['plant_type']+'</div></td>';
+					tbl +='<td ><div class="row_data" edit_type="click" col_name="plant_name">'+val['plant_name']+'</div></td>';
+                    tbl +='<td ><div class="row_data" edit_type="click" col_name="plant_price">'+val['plant_price']+'</div></td>';
+                    tbl +='<td ><div class="row_data" edit_type="click" col_name="plant_status">'+val['plant_status']+'</div></td>';
+
+					//--->edit options > start
+					tbl +='<td>';
+					 
+						tbl +='<span class="btn_edit" > <a href="#" class="btn btn-link " row_id="'+row_id+'" > Edit</a> </span>';
+
+						//only show this button if edit button is clicked
+						tbl +='<span class="btn_save"> <a href="#" class="btn btn-link"  row_id="'+row_id+'"> Save</a> | </span>';
+						tbl +='<span class="btn_cancel"> <a href="#" class="btn btn-link" row_id="'+row_id+'"> Cancel</a> | </span>';
+
+					tbl +='</td>';
+					//--->edit options > end
+					
+				tbl +='</tr>';
+			});
+
+			//--->create table body rows > end
+
+		tbl +='</tbody>';
+		//--->create table body > end
+
+	tbl +='</table>'	
+	//--->create data table > end
+
+	//out put table data
+	$(document).find('.tbl_user_data').html(tbl);
+
+	$(document).find('.btn_save').hide();
+	$(document).find('.btn_cancel').hide(); 
+
+
+	//--->make div editable > start
+	$(document).on('click', '.row_data', function(event) 
+	{
+		event.preventDefault(); 
+
+		if($(this).attr('edit_type') == 'button')
+		{
+			return false; 
+		}
+
+		//make div editable
+		$(this).closest('div').attr('contenteditable', 'true');
+		//add bg css
+		$(this).addClass('bg-warning').css('padding','5px');
+
+		$(this).focus();
+	})	
+	//--->make div editable > end
+
+
+	//--->save single field data > start
+	$(document).on('focusout', '.row_data', function(event) 
+	{
+		event.preventDefault();
+
+		if($(this).attr('edit_type') == 'button')
+		{
+			return false; 
+		}
+
+		var row_id = $(this).closest('tr').attr('row_id'); 
+		
+		var row_div = $(this)				
+		.removeClass('bg-warning') //add bg css
+		.css('padding','')
+
+		var col_name = row_div.attr('col_name'); 
+		var col_val = row_div.html(); 
+
+		var arr = {};
+		arr[col_name] = col_val;
+
+		//use the "arr"	object for your ajax call
+		$.extend(arr, {row_id:row_id});
+
+		//out put to show
+		$('.post_msg').html( '<pre class="bg-success">'+JSON.stringify(arr, null, 2) +'</pre>');
+		
+	})	
+	//--->save single field data > end
+
+ 
+	//--->button > edit > start	
+	$(document).on('click', '.btn_edit', function(event) 
+	{
+		event.preventDefault();
+		var tbl_row = $(this).closest('tr');
+
+		var row_id = tbl_row.attr('row_id');
+
+		tbl_row.find('.btn_save').show();
+		tbl_row.find('.btn_cancel').show();
+
+		//hide edit button
+		tbl_row.find('.btn_edit').hide(); 
+
+		//make the whole row editable
+		tbl_row.find('.row_data')
+		.attr('contenteditable', 'true')
+		.attr('edit_type', 'button')
+		.addClass('bg-warning')
+		.css('padding','3px')
+
+		//--->add the original entry > start
+		tbl_row.find('.row_data').each(function(index, val) 
+		{  
+			//this will help in case user decided to click on cancel button
+			$(this).attr('original_entry', $(this).html());
+		}); 		
+		//--->add the original entry > end
+
+	});
+	//--->button > edit > end
+
+
+	//--->button > cancel > start	
+	$(document).on('click', '.btn_cancel', function(event) 
+	{
+		event.preventDefault();
+
+		var tbl_row = $(this).closest('tr');
+
+		var row_id = tbl_row.attr('row_id');
+
+		//hide save and cacel buttons
+		tbl_row.find('.btn_save').hide();
+		tbl_row.find('.btn_cancel').hide();
+
+		//show edit button
+		tbl_row.find('.btn_edit').show();
+
+		//make the whole row editable
+		tbl_row.find('.row_data')
+		.attr('edit_type', 'click')
+		.removeClass('bg-warning')
+		.css('padding','') 
+
+		tbl_row.find('.row_data').each(function(index, val) 
+		{   
+			$(this).html( $(this).attr('original_entry') ); 
+		});  
+	});
+	//--->button > cancel > end
+
+	
+	//--->save whole row entery > start	
+	$(document).on('click', '.btn_save', function(event) 
+	{
+		event.preventDefault();
+		var tbl_row = $(this).closest('tr');
+
+		var row_id = tbl_row.attr('row_id');
+
+		
+		//hide save and cacel buttons
+		tbl_row.find('.btn_save').hide();
+		tbl_row.find('.btn_cancel').hide();
+
+		//show edit button
+		tbl_row.find('.btn_edit').show();
+
+
+		//make the whole row editable
+		tbl_row.find('.row_data')
+		.attr('edit_type', 'click')
+		.removeClass('bg-warning')
+		.css('padding','') 
+
+		//--->get row data > start
+		var arr = {}; 
+		tbl_row.find('.row_data').each(function(index, val) 
+		{   
+			var col_name = $(this).attr('col_name');  
+			var col_val  =  $(this).html();
+			arr[col_name] = col_val;
+		});
+		//--->get row data > end
+
+		//use the "arr"	object for your ajax call
+		$.extend(arr, {row_id:row_id});
+
+		//out put to show
+		$('.post_msg').html( '<pre class="bg-success">'+JSON.stringify(arr, null, 2) +'</pre>')
+		 
+
+	});
+	//--->save whole row entery > end
+
+
+});
